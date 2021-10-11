29aee771fa3d9f9bad1d16ab54fe1d0b7373b17e
viktor nikolovski <72465925+Vichi2323@users.noreply.github.com>
2021-10-11 12:35:27 +0200

    editing characters page

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,25 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 import Characters from './Pages/Characters';
 import Episodes from './Pages/Episodes';
 import Locations from './Pages/Locations';
 import { ROUTES } from './Constants/Routes';
+import Nav from './Nav';
 
 
-import '../Assets/Styles/global.css';
 import '../Assets/Styles/App.css';
+import '../Assets/Styles/global.css';
 
 function App() {
   return (
     <BrowserRouter>
     	<div className="App">
         	<header className="App-header">
-            	Rick and Morty App
+            		<Link to={ROUTES.HOME} className="app-link">
+							Rick and Morty App
+					</Link>
         	</header>
+			<Nav />
 			<Switch>
 				<Route path={ROUTES.CHARACTERS} component={Characters} />
 				<Route path={ROUTES.EPISODES} component={Episodes} />
