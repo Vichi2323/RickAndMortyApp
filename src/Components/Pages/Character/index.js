fb17225d087a683afbee38c927bb27a048fef121
viktor nikolovski <72465925+Vichi2323@users.noreply.github.com>
2021-10-14 18:49:44 +0200

    edit character page

--- src/Components/Pages/Character/index.js
+++ src/Components/Pages/Character/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { getCharacter } from '../../Servises/RestAPI';
+import './character.css'
 
 const Character = () => {
 
@@ -17,8 +18,23 @@ const Character = () => {
     },[]);
 
     return (
-        <div>
-            
+        <div className="character-paige-container">
+            {character ? 
+                <div className="character-container">
+                    <div className="img-div">
+                        <img className="character-img" src={character.image} alt="character image"/>
+                    </div>
+                    <div className="character-info">
+                        <span>{character.gender}</span>
+                        <span>{character.location.name}</span>
+                        <span>{character.origin.name}</span>
+                        <span>{character.species}</span>
+                        <span>{character.status}</span>
+                        <span>{character.type}</span>
+                        
+                    </div>
+                </div>
+            :null}
         </div>
     );
 };
